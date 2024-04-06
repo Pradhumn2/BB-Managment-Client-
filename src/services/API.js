b0d17a5a0acd841607fae87fbf8b2f930a89a541
Pradhumn2 <pradhumnsharma2002@gmail.com>
2024-04-06 21:22:52 +0530

    updated

--- src/services/API.js
+++ src/services/API.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: process.env.REACT_APP_BASEURL });
+const API = axios.create({
+  baseURL: process.env.REACT_APP_BASEURL,
+  withCredentials: true,
+});
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("token")) {
     req.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
   }
+  // req.headers.credentials = true;
   return req;
 });
 
